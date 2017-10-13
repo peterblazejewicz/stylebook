2ec96fa9b7121571208a0dd3b74de669a841e1b0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-10-13 23:39:58 +0200

    Markdown transpiling support
    - markdown
    - code highlighter
    - build script (asynchronous)

--- build/markdown-it.js
+++ build/markdown-it.js
@@ -1,8 +1,16 @@
+const { promisify } = require('util');
+const fs = require('fs');
+const fse = require('fs-extra');
+const globAsync = promisify(require('glob'));
 const MarkdownIt = require('markdown-it');
+const path = require('path');
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+const argv = require('yargs').argv;
+
 const md = new MarkdownIt({
   html: true,
   highlight: (code) => {
-    console.log(code);
     if (code) {
       try {
         return hljs.highlightAuto(code).value;
@@ -12,16 +20,22 @@ const md = new MarkdownIt({
     return '';
   }
 });
-const code = `
-# Hello World!
 
-\`\`\`js
-class MyClass extends OtherClass {
-  constructor() {
-    super();
+const main = async ({input, output}) => {
+  try {
+    let files = await globAsync(input);
+    files.forEach(async file => {
+      let inputPath = path.join(process.cwd(), file);
+      let code = await readFileAsync(file, 'utf8');
+      let outPath = path.join(process.cwd(), output, file.replace('.md', '.html'));
+      let content = md.render(code);
+      await fse.ensureDir(path.dirname(outPath));
+      let success = await writeFileAsync(outPath, content, 'utf8');
+    });
+  } catch(error) {
+    console.error(error);
+    throw error;
   }
 }
-\`\`\`
-`
-const results = md.render(code);
-console.log(results);
+let [input, output] = argv._;
+main({input, output});
---
+++ docs/about/about.md
@@ -0,0 +1,5 @@
+# About Us
+
+## Details
+
+Details about us.
